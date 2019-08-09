35761d0eb38936093c5c4f0c183cdffae2f74313
huangzijian888 <huangzijian888@gmail.com>
2019-08-09 11:51:16 +0800

    初始化

---
+++ src/main/java/cn/huangzijian888/smstest/SmstestApplication.java
@@ -0,0 +1,13 @@
+package cn.huangzijian888.smstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmstestApplication.class, args);
+    }
+
+}
